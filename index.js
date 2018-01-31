85127eaea59d164be233d3bc0203e9a9a19df4ca
noneedsystem <noneedsystem@gmail.com>
2018-01-31 15:59:55 +0000

    environment issue solved

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express   = global.express = require('express');
 const app       = express();
 const Riothing  = true && require('riothing') || require('../_packs/riothing/index.js');
-//console.log(process.env.NODE_ENV);
 
 const CFG = {
   PORT: process.env.PORT  || 3001,
   IP:   process.env.IP    || '127.0.0.1',
   INIT_ACTION_NAME:       'INIT_APP',
   PUB_DIR:                __dirname + '/public',
-  DEV:                    process.env.NODE_ENV && process.env.NODE_ENV.indexOf('development') > -1
+  DEV:                    process.env.NODE_ENV === 'development'
 };
 
 Object.assign(CFG, {
